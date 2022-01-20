eddf97420d94057eead4211dbc614fde9fa5308c
bed-com-45-19 <bed-com-45-19@unima.ac.mw>
2022-01-20 00:43:52 -0800

    finish with swagger

--- src/main/java/com/example/online/boking/OnlineBokingApplication.java
+++ src/main/java/com/example/online/boking/OnlineBokingApplication.java
@@ -3,16 +3,35 @@ package com.example.online.boking;
 import com.example.online.boking.Customer.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
 
+
+import java.util.Collections;
 import java.util.List;
-
+//@EnableSwagger2
 @SpringBootApplication
 public class OnlineBokingApplication {
+//	@Bean
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineBokingApplication.class, args);
 	}
 
-}
+
+
+//	private ApiInfo apiDetails() {
+//		return new ApiInfo(
+//				"reg API",
+//				"API documentation",
+//				"1.0",
+//				"Free to use",
+//				new Contact("binary-code", "binary-code.blogspot,com", "binary-code"),
+//				"API licence",
+//				"",
+//				Collections.emptyList());
+
+
+	}
+
+
+
